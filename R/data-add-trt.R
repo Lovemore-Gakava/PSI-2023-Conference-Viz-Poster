fe06524cacef0f84dcbea015af15c4d6186a0ef9
Lovemore Gakava <Lovemore.Gakava@gmail.com>
2023-01-16 22:41:26 +0000

    Add treatment variable to WW data Write out treatment added RDS to data folder

---
+++ R/data-add-trt.R
@@ -0,0 +1,10 @@
+library(readr)
+library(tidyverse)
+set.seed(12345)
+dat <- read.csv("data-raw/PSI_WW_psychometric.csv")
+dat$rand <- sample(1:100, nrow(dat), replace = TRUE)
+dat <- dat |>
+  mutate(TRT = ifelse(rand < 50, "Treatment A", "Treatment B")) |>
+  select(-c("rand"))
+
+write_rds(data, "data/PSI_WW_psychometric.rds")
